cd5c66a64a02481df98c8db75ed376f4aee94665
d <d.mirauta@protonmail.com>
2023-08-17 12:44:45 +0100

    solver vars

--- lp_primal_dual_solver.c
+++ lp_primal_dual_solver.c
@@ -2,87 +2,97 @@
 
 // negative kkt equalities gap (changing epsilon)
 // res size is N + 2M
-void kkt_neg_res(LPDef_t *lp, FPN *x, FPN *u, FPN *v, FPN *res, FPN eps) {
+void kkt_neg_res(LPDef_t *lp, SolverVars_t *vars, FPN eps) {
   // primal feasibility gap (b-Ax)
   for (IDX i = 0; i < lp->N; i++) {
-    res[i] = lp->b_ptr[i];
+    vars->res[i] = lp->b_ptr[i];
     for (IDX j = 0; j < lp->M; j++) {
-      res[i] -= lp->A_ptr[i * lp->M + j] * x[j];
+      vars->res[i] -= lp->A_ptr[i * lp->M + j] * vars->x[j];
     }
   }
 
   // complementary slackness gap (x_i u_i = ε, u is lagrange mult for x_i >= 0)
   for (IDX i = 0; i < lp->M; i++) {
-    res[lp->N + i] = eps - x[i] * u[i];
+    vars->res[lp->N + i] = eps - vars->x[i] * vars->u[i];
   }
 
   // dual feasibility gap (c - A^T v - u, v is lagrange mult for Ax=b)
   for (IDX i = 0; i < lp->M; i++) {
-    res[lp->N + lp->M + i] = lp->c_ptr[i];
+    vars->res[lp->N + lp->M + i] = lp->c_ptr[i];
     for (IDX j = 0; j < lp->N; j++) {
-      res[i] -= v[j] * lp->A_ptr[j * lp->M + i];
-      res[i] -= u[i];
+      vars->res[i] -= vars->v[j] * lp->A_ptr[j * lp->M + i];
+      vars->res[i] -= vars->u[i];
     }
   }
 }
 
 // gradient is N + 2M, N + 2M (shape)
-void init_grad(LPDef_t *lp, FPN *x, FPN *u, FPN *grad) {
+void init_grad(LPDef_t *lp, SolverVars_t *vars) {
   IDX size = lp->N + 2 * lp->M;
 
   // init all to zero for simplicity
   for (IDX i = 0; i < size; i++) {
     for (IDX j = 0; j < size; j++) {
-      grad[i * size + j] = FZERO;
+      vars->grad[i * size + j] = FZERO;
     }
   }
 
   // top left is A
   for (IDX i = 0; i < lp->N; i++) {
     for (IDX j = 0; j < lp->M; j++) {
-      grad[i * size + j] -= lp->A_ptr[i * lp->M + j];
+      vars->grad[i * size + j] -= lp->A_ptr[i * lp->M + j];
     }
   }
 
   // mid left is diag u
   for (IDX j = 0; j < lp->M; j++) {
-    grad[(lp->N + j) * size + j] = u[j];
+    vars->grad[(lp->N + j) * size + j] = vars->u[j];
   }
 
   // mid mid is diag x
   for (IDX j = 0; j < lp->M; j++) {
-    grad[(lp->N + j) * size + (lp->M + j)] = x[j];
+    vars->grad[(lp->N + j) * size + (lp->M + j)] = vars->x[j];
   }
 
   // bot mid is I_M
   for (IDX j = 0; j < lp->M; j++) {
-    grad[(lp->N + lp->M + j) * size + (lp->M + j)] = FONE;
+    vars->grad[(lp->N + lp->M + j) * size + (lp->M + j)] = FONE;
   }
 
   // bot right is A^T
   for (IDX i = 0; i < lp->N; i++) {
     for (IDX j = 0; j < lp->M; j++) {
-      grad[(lp->N + lp->M + j) * size + (2 * lp->M + i)] =
+      vars->grad[(lp->N + lp->M + j) * size + (2 * lp->M + i)] =
           lp->A_ptr[i * lp->M + j];
     }
   }
 }
 
-void update_grad(LPDef_t *lp, FPN *x, FPN *u, FPN *grad) {
+// most of the gradient is constant, we only need to update middle blocks
+void update_grad(LPDef_t *lp, SolverVars_t *vars) {
   IDX size = lp->N + 2 * lp->M;
 
   // mid left is diag u
   for (IDX j = 0; j < lp->M; j++) {
-    grad[(lp->N + j) * size + j] = u[j];
+    vars->grad[(lp->N + j) * size + j] = vars->u[j];
   }
 
   // mid mid is diag x
   for (IDX j = 0; j < lp->M; j++) {
-    grad[(lp->N + j) * size + (lp->M + j)] = x[j];
+    vars->grad[(lp->N + j) * size + (lp->M + j)] = vars->x[j];
   }
 }
 
-// iterative rootfinding in the linearised kkt residual
-void solve() {
-  // TODO
+FPN L2(FPN *res, IDX size) {
+  FPN sq_diff = FZERO;
+
+  for (IDX i = 0; i < size; i++) {
+    sq_diff += res[i] * res[i];
+  }
+
+  return sq_diff;
 }
+
+// iterative rootfinding in the linearised kkt residual
+// solver variables will be allocated by caller
+void solve(LPDef_t *lp, SolverVars_t *vars, SolverOpt_t opt) {}
--- lp_primal_dual_solver.h
+++ lp_primal_dual_solver.h
@@ -13,3 +13,22 @@ typedef struct LPDef {
   // num variables
   IDX M;
 } LPDef_t;
+
+typedef struct SolverVars {
+  FPN *grad;
+  FPN *res;
+  FPN *x;
+  FPN *u;
+  FPN *v;
+} SolverVars_t;
+
+typedef struct SolverOpt {
+  // initial complementary slackness relaxation 'epsilon'
+  FPN eps;
+  // epsilon multiplier (per iter)
+  FPN eps_q;
+  // convergence tolerance
+  FPN tol;
+  // maximum iterations
+  IDX maxiter;
+} SolverOpt_t;
